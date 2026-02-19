1ead4f16b3f16e302c085cead29461ace87e1558
qunoot zehra <qunootzehra21@gmail.com>
2026-02-20 03:54:48 +0500

    chnageds

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,26 @@
 <?php
+session_start();
 include '../classes/auth.php';
 include '../config/connect.php';
-$database = 
+$database = (new database)->connection();
+$table = new auth($database);
+if(isset($_POST['login'])){
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $user = $table->login($email,$password);
+  if($user){
+    $_SESSION['username'] = $user['user_name'];
+    $_SESSION['email'] = $user['user_email'];
+    $_SESSION['role'] = $user['role_id'];
+    if($_SESSION['role']== 1){
+      header("location:../admin/dashboad.php");
+    }elseif()
+  }
+  else{
+    echo "<script>alert('not')</script>";
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -22,20 +41,20 @@ $database =
           class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form>
+        <form method="post" >
           
             <h1 class="lead fw-bold mb-0 mb-4 text-center">login in </h1>
 
           <!-- Email input -->
           <div data-mdb-input-init class="form-outline mb-4">
-            <input type="email" id="form3Example3" class="form-control form-control-lg"
+            <input type="email" name="email" id="form3Example3" class="form-control form-control-lg"
               placeholder="Enter a valid email address" />
             <label class="form-label" for="form3Example3">Email address</label>
           </div>
 
           <!-- Password input -->
           <div data-mdb-input-init class="form-outline mb-3">
-            <input type="password" id="form3Example4" class="form-control form-control-lg"
+            <input type="password" name="password" id="form3Example4" class="form-control form-control-lg"
               placeholder="Enter password" />
             <label class="form-label" for="form3Example4">Password</label>
           </div>
@@ -45,15 +64,14 @@ $database =
             <div class="form-check mb-0">
               <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
               <label class="form-check-label" for="form2Example3">
-                Remember me
+                
               </label>
             </div>
-            <a href="#!" class="text-body">Forgot password?</a>
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
-              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;" name="login">Login</button>
             <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="register.php"
                 class="link-danger">Register</a></p>
           </div>
--- classes/auth.php
+++ classes/auth.php
@@ -11,5 +11,16 @@ class auth{
         return $q->fetchAll(PDO::FETCH_ASSOC);
     }
     // public function logout()
+    public function login($email,$password){
+        $q = $this->db->prepare("SELECT * FROM users where user_email=?");
+        $q->execute([$email]);
+        $user = $q->fetch(PDO::FETCH_ASSOC);
+        if($user && password_verify($password,$user['user_password'])){
+            return $user;
+        }
+        
+            return false;
+        
+    }
 }
 ?>
