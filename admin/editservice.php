39e643f364f92cd03cf7dc41030d65fab062463e
qunoot zehra <qunootzehra21@gmail.com>
2026-02-24 00:18:06 +0500

    Change

--- admin/editservice.php
+++ admin/editservice.php
@@ -4,8 +4,18 @@ $conn = ( new database)->connection();
 $table = new services($conn);
 
 $id = $_GET['id'];
-if( $table->ediid($id)){
-    
+$data = $table->ediid($id);
+
+if(isset($_POST['edit'])){
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $image = $data['services_img'] ;
+    if(!empty($_FILES['image']['name'])){
+        $image = $_FILES['image']['name'];
+        $image_tmp = $_FILES['image']['tmp_name'];
+        move_uploaded_file($image_tmp ,"../asset/images/service".$image );
+    }
+
 }
 ?>
 <!DOCTYPE html>
@@ -21,15 +31,18 @@ if( $table->ediid($id)){
         <h1 class="m-auto mb-5 mt-5 col-5">Add New Service</h1>
         </div>
 <div class="row">
+    <?php
+    if($data){
+    ?>
     <form class="m-auto col-6" method="post" enctype="multipart/form-data">
   <div class="form-row">
     <div class="form-group col-md-12">
       <label for="inputEmail4">Service Name</label>
-      <input type="text"  class="form-control" id="inputEmail4" name="name" placeholder="service name">
+      <input type="text" value="<?=$data['service_name']?>"  class="form-control" id="inputEmail4" name="name" placeholder="service name">
     </div>
     <div class="form-group col-md-12">
       <label for="inputPassword4">Price</label>
-      <input type="number" class="form-control" name="price" id="inputPassword4" >
+      <input type="number" class="form-control" value="<?=$data['services_img']?>" name="price" id="inputPassword4" >
     </div>
   </div>
   <div class="form-row">
@@ -38,8 +51,9 @@ if( $table->ediid($id)){
       <input type="file" class="form-control" name="image" id="inputPassword4" >
     </div>
   </div>
-  <button type="submit" name="add" class="btn mt-4 btn-primary"> Add</button>
+  <button type="submit" name="edit" class="btn mt-4 btn-primary"> Edit</button>
 </form>
+<?php  } ?>
 </div>
 </body>
 </html>
